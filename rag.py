889cb2acaade9ef572e0af3f73c720b5d7d22742
nnngoc <kid.nguyenngoc@gmail.com>
2024-05-06 22:35:17 +0700

    update

--- rag.py
+++ rag.py
@@ -139,6 +139,7 @@ def format_docs(docs):
 from langchain.prompts import PromptTemplate
 template ="""
 Trả lời câu hỏi dựa trên những quy định được cung cấp.
+Tổng hợp thông tin và đưa ra câu trả lời đầy đủ thông tin cuối cùng.
 Không cần ghi chú và trích dẫn nguồn thông tin đã tham khảo trong câu trả lời.
 Câu trả lời nên bắt đầu bằng: "Theo quy định của Trường ĐH Bách Khoa Tp.HCM, ..."
 Nếu trong quy văn bản không có thông tin cho câu trả lời, vui lòng thông báo: "Xin lỗi, tôi không có thông tin cho câu hỏi này!"
